ae2701617c52e99f0d138f53383ec8be55267676
Nikolai.Zolotykh <Nikolai.Zolotykh@fc6cb6c1-6420-0410-bc37-230e6677d226>
2006-11-12 19:12:13 +0000

    version 01.00.01

--- @pointer/utils.h
+++ @pointer/utils.h
@@ -4,6 +4,8 @@ mxArray* RegisterObject(mxArray *strct, const char *obj_name);
 
 mxArray* MakeStructCopyAndCreateObject(int m, int n, int nfields, const char *field_names[], const char *obj_name);
 
+mxArray* MakeStructCopyAndCreatePointer(const mxArray *data);
+
 char* AllocAndGetString(const mxArray *A);
 
 void SetPointerData(mxArray *A, const mxArray *data);
